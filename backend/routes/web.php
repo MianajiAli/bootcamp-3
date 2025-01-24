6c697649f0514689e4f59fba53c6016927712286
Ali <87234097+MianajiAli@users.noreply.github.com>
2025-01-24 15:37:17 +0330

    Add Customer Api

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,9 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\PostController;
 
 
+Route::apiResource('posts', PostController::class);
+
+// Route::prefix('/v1/admin')->group(function () {
+//      Route::get('/', function () {
+//         return 'Admin home Page';
+//     });
+//     Route::get('/users', [ItemController::class, 'index']); // GET all items
+//     Route::post('/users', [ItemController::class, 'store']); // POST a new item
+//     Route::get('/settings', function () {
+//         return 'Admin Settings Page';
+//     });
+// });
