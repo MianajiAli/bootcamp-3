331b6ff8c10e39ab46c2d368193f3cff9958a07d
Ali <87234097+MianajiAli@users.noreply.github.com>
2025-01-28 13:18:30 +0330

    init

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SendmessageComponent } from './sendmessage/sendmessage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'send', component: SendmessageComponent }
+];
---
+++ frontend/src/app/sendmessage/sendmessage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sendmessage',
+  imports: [],
+  templateUrl: './sendmessage.component.html',
+  styleUrl: './sendmessage.component.scss'
+})
+export class SendmessageComponent {
+
+}
